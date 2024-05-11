70b6b48c1cc74cd6671c8556ebc64daff903ae6c
alan <alanliu@zego.im>
2024-05-11 15:55:42 +0800

    update

---
+++ zego_uikit_plugin_adapter/src/main/java/com/zegocloud/uikit/plugin/adapter/plugins/beauty/ZegoBeautyPluginInnerTextCHS.java
@@ -0,0 +1,138 @@
+package com.zegocloud.uikit.plugin.adapter.plugins.beauty;
+
+public class ZegoBeautyPluginInnerTextCHS extends ZegoBeautyPluginInnerText {
+
+    public ZegoBeautyPluginInnerTextCHS() {
+         titleBeautyBasic = "基础美颜";
+         titleFilter = "滤镜";
+         titleFilterNatural = "自然";
+         titleFilterGray = "灰调";
+         titleFilterDreamy = "梦境";
+         titleBeautify = "美颜";
+         titleStickers = "贴纸";
+         titleBackground = "背景";
+         titleBeautyAdvanced = "高级美颜";
+         titleBeautyMakeup = "美妆";
+         titleBeautyStyleMakeup = "风格妆";
+
+        // Beauty
+        // other
+         beautyReset = "重置";
+         beautyNone = "原图";
+
+        // Beauty - Basic
+         beautyBasicSmoothing = "磨皮";
+         beautyBasicSkinTone = "美白";
+         beautyBasicBlusher = "腮红";
+         beautyBasicSharpening = "锐化";
+         beautyBasicWrinkles = "去法令纹";
+         beautyBasicDarkCircles = "去黑眼圈";
+
+        // Beauty - Advanced
+         beautyAdvancedFaceSlimming = "瘦脸";
+         beautyAdvancedEyesEnlarging = "大眼";
+         beautyAdvancedEyesBrightening = "亮眼";
+         beautyAdvancedChinLengthening = "收下巴";
+         beautyAdvancedMouthReshape = "小嘴";
+         beautyAdvancedTeethWhitening = "白牙";
+         beautyAdvancedNoseSlimming = "瘦鼻";
+         beautyAdvancedNoseLengthening = "长鼻";
+         beautyAdvancedFaceShortening = "小脸";
+         beautyAdvancedMandibleSlimming = "瘦下颌骨";
+         beautyAdvancedCheekboneSlimming = "瘦颧骨";
+         beautyAdvancedForeheadSlimming = "收额头";
+
+        // Beauty - Makeup
+        // Beauty - Makeup - Lipstick
+         beautyMakeupLipstick = "口红";
+         beautyMakeupLipstickCameoPink = "豆沙粉";
+         beautyMakeupLipstickSweetOrange = "甜橘色";
+         beautyMakeupLipstickRustRed = "铁锈红";
+         beautyMakeupLipstickCoral = "珊瑚色";
+         beautyMakeupLipstickRedVelvet = "丝绒红";
+
+        // Beauty - Makeup - Blusher
+         beautyMakeupBlusher = "腮红";
+         beautyMakeupBlusherSlightlyDrunk = "微醺";
+         beautyMakeupBlusherPeach = "蜜桃";
+         beautyMakeupBlusherMilkyOrange = "奶橘";
+         beautyMakeupBlusherApricotPink = "杏粉";
+         beautyMakeupBlusherSweetOrange = "甜橙";
+
+        // Beauty - Makeup - Eyelashes
+         beautyMakeupEyelashe = "眼睫毛";
+         beautyMakeupEyelashesNatural = "自然";
+         beautyMakeupEyelashesTender = "温柔";
+         beautyMakeupEyelashesCurl = "卷翘";
+         beautyMakeupEyelashesEverlong = "纤长";
+         beautyMakeupEyelashesThick = "浓密";
+
+        // Beauty - Makeup - Eyeliner
+         beautyMakeupEyeliner = "眼线";
+         beautyMakeupEyelinerNatural = "自然";
+         beautyMakeupEyelinerCatEye = "野猫";
+         beautyMakeupEyelinerNaughty = "俏皮";
+         beautyMakeupEyelinerInnocent = "心机";
+         beautyMakeupEyelinerDignified = "气质";
+
+        // Beauty - Makeup - Eyeshadow
+         beautyMakeupEyeshadow = "眼影";
+         beautyMakeupEyeshadowPinkMist = "粉雾海";
+         beautyMakeupEyeshadowShimmerPink = "微光蜜";
+         beautyMakeupEyeshadowTeaBrown = "暖茶棕";
+         beautyMakeupEyeshadowBrightOrange = "元气橙";
+         beautyMakeupEyeshadowMochaBrown = "摩卡棕";
+
+        // Beauty - Makeup - Colored Contacts
+         beautyMakeupColoredContacts = "美瞳";
+         beautyMakeupColoredContactsDarknightBlack = "水光黑";
+         beautyMakeupColoredContactsStarryBlue = "星空蓝";
+         beautyMakeupColoredContactsBrownGreen = "仙棕绿";
+         beautyMakeupColoredContactsLightsBrown = "异瞳棕";
+         beautyMakeupColoredContactsChocolateBrown = "可可棕";
+
+        // Beauty - Style-Makeup
+         beautyStyleMakeupInnocentEyes = "眼睑下至妆";
+         beautyStyleMakeupMilkyEyes = "银河眼妆";
+         beautyStyleMakeupCutieCool = "奶凶";
+         beautyStyleMakeupPureSexy = "纯欲";
+         beautyStyleMakeupFlawless = "神颜";
+
+        // Filters
+        // Filters - Natural
+         filterNaturalCreamy = "奶油";
+         filterNaturalBrighten = "青春";
+         filterNaturalFresh = "清新";
+         filterNaturalAutumn = "秋天";
+
+        // Filters - Gray
+         filterGrayMonet = "莫奈";
+         filterGrayNight = "暗夜";
+         filterGrayFilmlike = "胶片";
+
+        // Filters - Dreamy
+         filterDreamySunset = "落日";
+         filterDreamyCozily = "琉璃";
+         filterDreamySweet = "星云";
+
+        // Stickers
+         stickerAnimal = "动物";
+         stickerDive = "潜水镜";
+         stickerCat = "猫头";
+         stickerWatermelon = "西瓜";
+         stickerDeer = "小鹿";
+         stickerCoolGirl = "酷炫脸模";
+         stickerClown = "小丑";
+         stickerClawMachine = "娃娃机";
+         stickerSailorMoon = "美少女战士";
+
+        // background
+         backgroundGreenScreenSegmentation = "绿幕分割";
+         backgroundPortraitSegmentation = "人像分割";
+         backgroundMosaicing = "马赛克";
+         backgroundMosaicingTriangle = "三角形";
+         backgroundMosaicingQuadrangle = "四边形";
+         backgroundMosaicingHexagon = "六边形";
+         backgroundGaussianBlur = "高斯模糊";
+    }
+}
---
+++ zego_uikit_plugin_adapter/src/main/java/com/zegocloud/uikit/plugin/adapter/plugins/beauty/ZegoBeautyPluginInnerTextEnglish.java
@@ -0,0 +1,5 @@
+package com.zegocloud.uikit.plugin.adapter.plugins.beauty;
+
+public class ZegoBeautyPluginInnerTextEnglish extends ZegoBeautyPluginInnerText {
+
+}
